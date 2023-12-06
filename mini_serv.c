80f0145f92e1753b91e31d1e693144ce721550b6
AnaAndric <90210982+Noushhhh@users.noreply.github.com>
2023-12-06 20:47:50 +0100

    Update mini_serv.c

--- mini_serv.c
+++ mini_serv.c
@@ -7,103 +7,126 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define ARGS "Wrong number of arguments\n"
-#define BUFFER_SIZE 2000000
-#define FATAL "Fatal error\n"
-#define MAX_PORTS 65000
+// DEFINE CONSTANTS
+#define	ARGS	"Wrong number of arguments\n"
+#define BUFF	200000
+#define	FATAL	"Fatal error\n"
+#define PORTS	65000
 
-int highest_fd, server_fd, current_id = 0;
-int clients[MAX_PORTS];
-char message_buffer[BUFFER_SIZE + 100];
-fd_set read_fds, write_fds, active_fds;
+// DECLARE GLOBAL VARIABLES
+int		maxfd, sockfd, id = 0;
+int		client_id_arr[PORTS];
+char	msg_buff[BUFF + 100];
+fd_set	rset, wset, aset;
 
-void printError(char *msg)
+// FUNCTION TO HANDLE ERRORS
+void	error(char *msg)
 {
-    if (server_fd > 2)
-        close(server_fd);
-    write(2, msg, strlen(msg));
-    exit(1);
+	if (sockfd > 2)
+		close(sockfd);
+	write(2, msg, strlen(msg));
+	exit(1);
 }
 
-void replyToAll(int sender_fd)
+// FUNCTION TO SEND A MESSAGE TO ALL CONNECTED CLIENT, EXCEPT THE SENDER
+void	replyToAll(int connfd)
 {
-	for (int fd = 2; fd <= highest_fd; fd++)
-		if (fd != sender_fd && FD_ISSET(fd, &write_fds))
-			if (send(fd, message_buffer, strlen(message_buffer), 0) < 0)
-				handle_error(FATAL);
+	for (int fd = 2; fd <= maxfd; fd++)
+		if (fd != connfd && FD_ISSET(fd, &wset))
+			if (send(fd, msg_buff, strlen(msg_buff), 0) < 0)
+				error(FATAL);
 }
 
 int main(int ac, char **av)
 {
-    int         client_fd;
-    socklen_t   addr_len;
-    struct      sockaddr_in server_addr, cli_addr;
+	int					connfd;
+	socklen_t			len;
+	struct sockaddr_in	servaddr, cliaddr; 
 
-    if (ac != 2)
-        error(ARGS);
+	// CHECK THE NUMBER OF ARGUMENTS
+	if (ac != 2)
+		error(ARGS);
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1)
-        error(FATAL);
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-    server_addr.sin_port = htons(atoi(av[1]));
+	// CREATE SOCKET AND CHECK FOR ERRORS 
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+	if (sockfd == -1)
+		error(FATAL);
 
-    if (bind(server_fd, (const struct sockaddr *) &server_addr, sizeof(server_addr)) != 0)
-        error(FATAL);
-    
-    if (listen(server_fd, 4096) != 0)
-        error(FATAL);
+	// SET SERVER'S IP ADDRESS & PORT NUMBER 
+	bzero(&servaddr, sizeof(servaddr)); 
+	servaddr.sin_family		 = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(2130706433); // 127.0.0.1
+	servaddr.sin_port		 = htons(atoi(av[1]));
 
-    addr_len = sizeof(cli_addr);
-    highest_fd = server_fd;
-    FD_ZERO(&active_fds);
-    FD_SET(server_fd, &active_fds);
+	// BIND THE SERVER SOCKET TO IP ADDRESS AND PORT
+	if (bind(sockfd, (const struct sockaddr*)&servaddr, sizeof(servaddr)) != 0)
+		error(FATAL);
 
-    while (1)
-    {
-        read_fds = write_fds = active_fds;
-        if (select(highest_fd + 1, &read_fds, &write_fds, 0, 0) < 0)
-            continue;
+	// LISTEN FOR INCOMING CLIENT CONNECTIONS
+	if (listen(sockfd, 4096) != 0)
+		error(FATAL);
 
-        if (FD_ISSET(server_fd, &read_fds))
-        {
-            client_fd = accept(server_fd, (struct sockaddr *) &cli_addr, &addr_len);
-            if (client_fd < 0)
-                printError(FATAL);
-            
-            sprintf(message_buffer, "server: client %d just arrived\n", current_id);
-            clients[client_fd] = current_id++;
-            FD_SET(client_fd, &active_fds);
-            replyToAll(client_fd);
-            highest_fd = (client_fd > highest_fd) ? client_fd : highest_fd ;
-            continue;
-        }
+	// START THE SERVER LOOP
+	len		= sizeof(cliaddr);
+	maxfd	= sockfd;
+	FD_ZERO(&aset);
+	FD_SET(sockfd, &aset);
 
-        for (int fd = 2; fd <= highest_fd; fd++)
-        {
-            if (FD_ISSET(fd, &read_fds))
-            {
-                int bytes_received = 1;
-                char msg[BUFFER_SIZE];
-                memset(&msg, 0, sizeof(msg));
+	while (1)
+	{
+		// SET UP THE FD SETS FOR THE select() FUNCTION
+		rset = wset = aset;
+		if (select(maxfd + 1, &rset, &wset, 0, 0) < 0)
+			continue;
 
-                while (bytes_received == 1 && msg[strlen(msg) - 1] != '\n')
-                    bytes_received = recv(fd, msg + strlen(msg), 1, 0);
-                
-                if (bytes_received <= 0)
-                {
-                    sprintf(message_buffer, "server: client %d just left \n", clients[fd]);
-                    FD_CLR(fd, &active_fds);
-                    close(fd);
-                }
-                else
-                    sprintf(message_buffer, "client %d: %s", clients[fd], msg);
+		// CHECK IF THERE IS A NEW INCOMING CLIENT CONNECTION
+		if (FD_ISSET(sockfd, &rset))
+		{
+			// ACCEPT NEW CONNECTION AND ADD IT TO FD_SET
+			connfd = accept(sockfd, (struct sockaddr *)&cliaddr, &len);
+			if (connfd < 0)
+				error(FATAL);
 
-                replyToAll(fd);
-            }
-        }
-    }
-    return (0);
+			// UPDATE THE MESSAGE BUFFER TO NOTIFY ABOUT NEW CLIENT 
+			sprintf(msg_buff, "server: client %d just arrived\n", id);
+
+			// UPDATE THE CLIENT ID ARRAY
+			client_id_arr[connfd] = id++;
+			FD_SET(connfd, &aset);
+
+			// SEND THE ARRIVAL MESSAGE TO ALL CONNECTED CLIENTS
+			replyToAll(connfd);
+			maxfd = (connfd > maxfd) ? connfd : maxfd;
+			continue;
+		}
+
+		// ITERATE THROUGH ALL CONNECTED CLIENTS TO CHECK FOR INCOMING MESSAGES
+		for (int fd = 2; fd <= maxfd; fd++)
+		{
+			if (FD_ISSET(fd, &rset))
+			{
+				int		recv_return = 1;
+				char	msg[BUFF];
+				bzero(&msg, sizeof(msg));
+
+				// READ A MESSAGE FROM THE CLIENT UNTIL A NEW LINE IS FOUND
+				while (recv_return == 1 && msg[strlen(msg) - 1] != '\n')
+					recv_return = recv(fd, msg + strlen(msg), 1, 0);
+				if (recv_return <= 0)
+				{
+					// UPDATE THE MESSAGE IF THE CLIENT DISCONNECTS OR AN ERROR OCCURS
+					sprintf(msg_buff, "server: client %d just left\n", client_id_arr[fd]);
+					FD_CLR(fd, &aset);
+					close(fd);
+				}
+				else
+					// UPDATE THE MESSAGE IF A MESSAGE IS RECEIVED
+					sprintf(msg_buff, "client %d: %s", client_id_arr[fd], msg);
+
+				// SEND THE DEPARTURE MESSAGE OR REGULAR MESSAGE TO ALL OTHER CONNECTED CLIENTS
+				replyToAll(fd);
+			}
+		}
+	}
+	return (0);
 }
